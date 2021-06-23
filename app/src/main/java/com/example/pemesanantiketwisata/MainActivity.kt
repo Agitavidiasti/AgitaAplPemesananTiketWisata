bb2159b7997d9ed2a68e762881d396968da06233
Agitavidiasti <agitavidiastirivallinata@gmail.com>
2021-06-22 21:56:32 -0700

    ViewPager
    ViewPager

--- app/src/main/java/com/example/pemesanantiketwisata/MainActivity.kt
+++ app/src/main/java/com/example/pemesanantiketwisata/MainActivity.kt
@@ -2,10 +2,23 @@ package com.example.pemesanantiketwisata
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.content.res.ResourcesCompat
+import com.google.android.material.tabs.TabLayoutMediator
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    val menuTeks = arrayOf("Home", "Pay", "History", "Saya")
+    val menuIcon = arrayOf(R.drawable.ic_home, R.drawable.ic_wallet, R.drawable.ic_history, R.drawable.ic_saya)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val adapter = ViewPagerAdapter(this)
+        view_pager.setAdapter(adapter)
+        TabLayoutMediator(tab_layout, view_pager, TabLayoutMediator.TabConfigurationStrategy{ tab, position ->
+            tab.text=menuTeks[position]
+            tab.icon= ResourcesCompat.getDrawable(resources, menuIcon[position], null)
+        }).attach()
     }
 }
